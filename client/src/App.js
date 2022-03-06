822434e42cd005c50ce6ab6739a6b685d330ca7d
T-Wesst <tyroo.west@sony.com>
2022-03-05 22:54:58 -0800

    add pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Sidenav from './components/Sidenav';
 import './App.css'
 import Landing from "./pages/Landing";
@@ -10,9 +14,13 @@ function App() {
     <>
     <Sidenav user="Tyroo" logo="Popup Foodies"/>
     <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route element={<ProtectedRoutes/>}>
-        <Route path="/" element={<Home />} />
-        <Route path="/landing" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/cart" element={<Cart />} />
       </Route>
     </Routes>
     </>
--- client/src/components/SidenavItems.js
+++ client/src/components/SidenavItems.js
@@ -1,25 +1,37 @@
 import React from 'react'
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
+import * as IoIcons from 'react-icons/io5';
 
 export const SidenavItems = [
     {
         title:'Home',
-        path: '/',
+        path: '/home',
         icon: <AiIcons.AiFillHome/>,
         cName: 'navText'
     },
     {
-        title:'Landing',
-        path: '/landing',
-        icon: <AiIcons.AiFillHome/>,
+        title:'My Profile',
+        path: '/profile',
+        icon: <FaIcons.FaUserCircle/>,
         cName: 'navText'
     },
     {
-        title:'Home',
-        path: '/',
-        icon: <AiIcons.AiFillHome/>,
+        title:'My Cart',
+        path: '/cart',
+        icon: <AiIcons.AiOutlineShoppingCart/>,
+        cName: 'navText'
+    },
+    {
+        title:'Log Out',
+        path: '/logout',
+        icon: <IoIcons.IoLogOutOutline/>,
+        cName: 'navText'
+    },
+    {
+        title:'View Map',
+        path: '/map',
+        icon: <FaIcons.FaMapMarkerAlt/>,
         cName: 'navText'
     },
 ];
