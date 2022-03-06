5e1a4752aa996ade34c4132680225fbd449d3968
T-Wesst <tyroo.west@sony.com>
2022-03-05 22:26:40 -0800

    protect routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import Home from './pages/Home';
 import Sidenav from './components/Sidenav';
 import './App.css'
 import Landing from "./pages/Landing";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 function App() {
   return (
     <>
     <Sidenav user="Tyroo" logo="Popup Foodies"/>
     <Routes>
+      <Route element={<ProtectedRoutes/>}>
         <Route path="/" element={<Home />} />
         <Route path="/landing" element={<Landing />} />
+      </Route>
     </Routes>
     </>
   );
